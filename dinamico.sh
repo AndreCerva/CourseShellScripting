29b7da7f020e47f8fe309016b869ac569c3b7ed7
Carlos Cervantes <85979189+AndreCerva@users.noreply.github.com>
2024-03-25 15:53:05 -0700

    Update dinamico.sh

--- dinamico.sh
+++ dinamico.sh
@@ -7,5 +7,6 @@ var2=$2
 echo "please introduce your name and age"
 read -p "Name: " name
 read -p "Age: " age
-echo "Hello, $name with $age years old, the current time is: $now"
-echo "the numbers that you gave to us are: $var1 and $var2 \n the sum of these are: $(($var1+$var2))"
+echo "Hello, $name of $age years old, the current time is: $now"
+echo "the numbers that you gave to us are: $var1 and $var2" 
+echo "the sum of these are: $(($var1+$var2))"
