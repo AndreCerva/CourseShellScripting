4f0036f6780e822296f514ec1dc6e8b4769c6456
andrecerva <carloscervantesloyola@gmail.com>
2024-03-25 22:23:25 -0700

    se agrega condiconal de archivo existente

--- ifstate.sh
+++ ifstate.sh
@@ -3,6 +3,7 @@
 numero1=0
 numero2=0
 
+
 read -p "Introduzca el primer numero " numero1
 read -p "Introduzca el segundo numero " numero2
 
@@ -12,3 +13,17 @@ then
 else
 	echo "El número $numero2 es mayor que $numero1"
 fi
+
+echo "Verificar que exista un archivo en el directorio actual"
+path=$(pwd)"/myfiletest.sh"
+echo $path
+if [ -f $path ]
+then 
+	echo "existe el archivo"
+else
+	echo "no existe el archivo"
+fi
+
+
+
+
